c050dcf80e52adb9b46272517ec0c3926d23a567
Aman Kumar <kumaraman140795@gmail.com>
2018-12-10 01:36:12 +0530

    Modified the style for todo-list

--- src/js/components/AddTodoForm.js
+++ src/js/components/AddTodoForm.js
@@ -1,8 +1,7 @@
 const React = require("react");
 
 class AddTodoForm extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.input = null;
   }
@@ -10,17 +9,29 @@ class AddTodoForm extends React.Component {
   render() {
     return (
       <div id="controls">
-      <input type="text" autoFocus id="todo-input" placeholder='Enter todo to add' ref={node => (this.input = node)} />
-      <button
-        className="btn btn-add"
-        onClick={() => {
-          this.props.addTodo(this.input.value);
-          this.input.value = "";
-        }}
-      >
-        Add
-      </button>
-    </div>
+        <input
+          type="text"
+          autoFocus
+          id="todo-input"
+          placeholder="Enter todo to add"
+          ref={node => (this.input = node)}
+          onKeyDown={event => {
+            if (event.keyCode === 13) {
+              this.props.addTodo(this.input.value);
+              this.input.value = "";
+            }
+          }}
+        />
+        <button
+          className="btn btn-add"
+          onClick={() => {
+            this.props.addTodo(this.input.value);
+            this.input.value = "";
+          }}
+        >
+          Add
+        </button>
+      </div>
     );
   }
 }
--- src/js/components/TodoItem.js
+++ src/js/components/TodoItem.js
@@ -2,12 +2,20 @@ const React = require("react");
 
 class TodoItem extends React.Component {
   render() {
-    const {todo, removeTodo, toggleTodo} = this.props;
+    const { todo, removeTodo, toggleTodo } = this.props;
     return (
-      <li className="todo-item">
+      <li
+        className="todo-item"
+        style={{
+          backgroundColor: todo.completed ? "rgb(224, 220, 234)" : "white"
+        }}
+      >
         <span
           className="fa fa-check todo-check"
           onClick={() => toggleTodo(todo.id)}
+          style={{
+            border: todo.completed ? "1px solid green" : "2px solid grey"
+          }}
         />
         <span
           className="todo-text"
