e3a2b6042e0b1fc811fdf925e9b99428401499b1
Aman Kumar <kumaraman140795@gmail.com>
2018-12-09 18:34:28 +0530

    Added reducers for todo and visibility state filter

--- src/js/actions/actionCreator.js
+++ src/js/actions/actionCreator.js
@@ -3,6 +3,11 @@ function generateId(){
   return String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Date.now();
 }
 
+// creating constants for different actions...
+const ADD_TODO = 'ADD_TODO',
+  TOGGLE_TODO = 'TOGGLE_TODO',
+  REMOVE_TODO = 'REMOVE_TODO',
+  SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 // this function returns an action object for adding a todo item...
 function addTodoAction(text){
@@ -29,11 +34,24 @@ function removeTodoAction(id){
   }
 }
 
+// this function returns the action object for SET_VISIBILITY_FILTER_ACTION...
+function setVisibiliyFilterAction(filter){
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter: filter
+  }
+}
+
 // creating an object with references to all the methods and returning it as the public API for this module...
 const Action = {
   addTodoAction,
   toggleTodoAction,
-  removeTodoAction
+  removeTodoAction,
+  setVisibiliyFilterAction,
+  ADD_TODO,
+  TOGGLE_TODO,
+  REMOVE_TODO,
+  SET_VISIBILITY_FILTER
 };
 
 module.exports = Action;
--- src/js/reducers/reducer.js
+++ src/js/reducers/reducer.js
@@ -1,25 +1,55 @@
-// this is the principal reducer for the app right now and later it will be replaced by a call to Redux.combineReducers() api...
+const Action = require('./../actions/actionCreator.js');
 
-// creating the initialState for the app, and later this will be reducer by what ever we fetch from the local storage, and thus we will be able to persist the data in our app...
-const initialState = {
-  todos: [
-    {
-      id: 0,
-      text: "Be the best or don't exist",
-      complete: false
-    },
-    {
-      id: 1,
-      text: "Walk the dog",
-      complete: false
-    }
-  ],
-  visibilityFilter: "SHOW_ALL"
-};
+// reducer for the todo items...
+function todosReducer(state = [], action) {
+  switch (action.type) {
+    case Action.ADD_TODO:
+      return state.concat({
+        id: action.id,
+        text: action.text,
+        completed: false
+      });
+      break;
 
-// for now, this reducer just relays back the state to the dispatch() of the store...
-function reducer(state = initialState, action) {
-  return state;
+    case Action.TOGGLE_TODO:
+      return state.map(todo => {
+        return todo.id == action.id
+          ? Object.assign({}, todo, { completed: !todo.completed })
+          : todo;
+      });
+      break;
+
+    case Action.REMOVE_TODO:
+      return state.filter(todo => {
+        return todo.id != action.id;
+      });
+      break;
+
+    default:
+      return state;
+      break;
+  }
+}
+
+// reducer for the visibility filter...
+function visibilityFilterReducer(state = "SHOW_ALL", action) {
+  switch (action.type) {
+    case Action.SET_VISIBILITY_FILTER:
+      return action.type;
+      break;
+
+    default:
+      return state;
+      break;
+  }
+}
+
+// this is the principal reducer function which calls the reducers for todos and visbility filter...
+function reducer(state = {}, action) {
+  return {
+    todos: todosReducer(state.todos, action),
+    visibilityFilter: visibilityFilterReducer(state.visibilityFilter, action)
+  };
 }
 
 module.exports = reducer;
